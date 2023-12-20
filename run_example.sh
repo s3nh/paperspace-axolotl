361a73dd9b6d7a8702508aaead5ac53c782b5539
s3nh <17111860+s3nh@users.noreply.github.com>
2023-12-20 10:23:31 +0100

    Update run_example.sh

--- run_example.sh
+++ run_example.sh
@@ -6,5 +6,8 @@ pip install -U transformers
 pip install -U accelerate
 pip install -e '.[flash-attn,deepspeed]' -q
 pip install -U flash_attn
+!pip install -U accelerate optimum
+#transformers_stream_generator needed if you want to finetune qwen
+!pip install transformers_stream_generator
 cp -r /notebooks/axolotl/src/* /usr/local/lib/python3.9/dist-packages/
 accelerate launch -m axolotl.cli.train examples/mamba/config.yml
